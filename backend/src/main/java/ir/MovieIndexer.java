d738845d8108e5a828bfbd5b2056a65e53bf3c15
bulajicj <bjovana1305.nk@gmail.com>
2023-06-21 16:38:23 +0200

    Movie Indexer

--- backend/src/main/java/ir/MovieIndexer.java
+++ backend/src/main/java/ir/MovieIndexer.java
@@ -48,7 +48,7 @@ public class MovieIndexer {
         document.add(new TextField("title", movie.getTitle(), Field.Store.YES));
         document.add(new TextField("cast", movie.getCast(), Field.Store.YES));
         document.add(new TextField("genres", movie.getGenres(), Field.Store.YES));
-        document.add(new TextField("plot", movie.getPlot(), Field.Store.YES));
+        document.add(new TextField("plot", movie.getPlot()));
         document.add(new IntPoint("releaseYear", movie.getReleaseYear()));
         document.add(new StoredField("releaseYear", movie.getReleaseYear()));
         document.add(new StoredField("wikiPage", movie.getWikiPage()));
